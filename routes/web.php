a5d18381e0f2bb5dde3d006762896a29cae6fc87
StartappzDev <startappzlimited01@gmail.com>
2024-09-25 09:54:19 +0300

    csrf and forms

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,15 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->simplePaginate(10);
-    return view('jobs', [
+    $jobs = Job::with('employer')->latest()->simplePaginate(10);
+    return view('jobs.index', [
         'jobs' => $jobs
     ]);
 });
 
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+});
 Route::get('/jobs/{id}', function ($id) {
 
 
@@ -28,7 +31,17 @@ Route::get('/jobs/{id}', function ($id) {
     //dd($job);
 
     // Optionally return a view here if needed (replace 'contact' with your intended view)
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
+});
+Route::post('/jobs', function () {
+    // return request()->all();
+    //validation
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1,
+    ]);
+    return redirect('/jobs');
 });
 
 Route::get('/contact', function () {
